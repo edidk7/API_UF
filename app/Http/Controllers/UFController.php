60361f22111a29946fd33d1a119893cdc8dc6882
Daniel Duarte <d.ignacio292@gmail.com>
2024-03-11 16:58:11 -0300

    Get all data Endpoint

--- app/Http/Controllers/UFController.php
+++ app/Http/Controllers/UFController.php
@@ -3,15 +3,21 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use GuzzleHttp\Client;
+use App\Models\UF;
 
 class UFController extends Controller
 {
-    public function obtenerDatosAPI()
+
+    protected $uf;
+    public function __construct(){
+        $this->uf = new UF();
+    }
+
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
     {
-        $client = new Client();
-        $response = $client->get('https://mindicador.cl/api/uf');
-        $data = json_decode($response->getBody(), true);
-        return $data;
+        return $this->uf->all();
     }
 }
